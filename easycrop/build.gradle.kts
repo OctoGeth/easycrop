b68d2ae92b476678faa4a97b29eccbf2bd6a9e29
theo-germain <theo.germain@octo.com>
2023-11-27 14:58:49 +0100

    jitpack config

--- easycrop/build.gradle.kts
+++ easycrop/build.gradle.kts
@@ -51,68 +51,21 @@ dependencies {
     androidTestImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
-fun extProperty(key: String): String {
-    return (extra.properties[key] ?: error("property $key not found in extras")) as String
-}
-
 publishing {
-    val data = object {
-        val groupId = "io.github.mr0xf00"
-        val artifactId = "easycrop"
-        val version = "0.1.1"
-        private val releasesRepoUrl =
-            uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-        private val snapshotsRepoUrl =
-            uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-        val repoUrl = if (version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-    }
-
     publications {
         register<MavenPublication>("release") {
-            groupId = data.groupId
-            artifactId = data.artifactId
-            version = data.version
+            groupId = "com.github.OctoGeth"
+            artifactId = "easycrop"
+            version = "0.1.1"
+
             afterEvaluate {
                 from(components["release"])
             }
-            pom {
-                name.set("EasyCrop")
-                description.set("Image cropper for jetpack compose")
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-                url.set("https://github.com/mr0xf00/easycrop")
-                scm {
-                    connection.set("https://github.com/mr0xf00/easycrop.git")
-                    url.set("https://github.com/mr0xf00/easycrop")
-                }
-                developers {
-                    developer {
-                        name.set("mr0xf00")
-                        email.set("mr0xf00@proton.me")
-                    }
-                }
-            }
         }
     }
-    publications {
-        repositories {
-            maven {
-                name = "oss"
-                url = data.repoUrl
-                credentials {
-                    username = extProperty("ossrh.user")
-                    password = extProperty("ossrh.pass")
-                }
-            }
-        }
-    }
-}
 
-signing {
-    sign(publishing.publications)
+    singleVariant("release") {
+        withSourcesJar()
+        withJavadocJar()
+    }
 }
