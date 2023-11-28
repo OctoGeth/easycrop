f6c695848d10fd96fc325b1c7ce92d3fa64a9f89
theo-germain <theo.germain@octo.com>
2023-11-28 11:11:11 +0100

    jitpack config

--- easycrop/build.gradle.kts
+++ easycrop/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
                 create<MavenPublication>("release") {
                     from(components["release"])
 
-                    groupId = "com.github.octogeth"
+                    groupId = "com.github.OctoGeth"
                     artifactId = "easycrop"
-                    version = "0.1.12"
+                    version = "0.1.13"
                 }
             }
         }
